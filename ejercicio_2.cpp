0e41982aedc3799004af2912f105bffad843b7f6
lquispele <lquispele@unjbg.edu.pe>
2024-06-23 14:53:19 -0500

    Actualizacion 1 del ejercicio 2: Acomando la interfaz haciendo que los valores que se representen sean mayores a 0 y usando la libreria unistd para esperar el resultado.

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include<unistd.h>
 using namespace std;
 int main(){
     int n1,n2,c=0, i=1;
-    cin>>n1;
-    cin>>n2;
+    do{
+        cout<<"Ingrese lo numeros que van a ser comparados: \n";
+        cin>>n1;    //el primer numero
+        cin>>n2;    // el segundo numero
+
     while(i<=n1&&i<=n2){
         if(n1%i==0&&n2%i==0){
             c++;
@@ -11,9 +15,11 @@ int main(){
         i++;
     }
     if(c>1){
-        cout<<"No son PESI";
+        cout<<"No son PESI"<<endl;
     } else{
-        cout<<"Son PESI";
+        cout<<"Son PESI"<<endl;
     }
-
+    sleep(2);
+    system("clear");
+    }while (n1>0&&n2>0);
 }
