e5e6fb79b1d02876f18cecd02e6e0a35e010d672
lquispele <lquispele@unjbg.edu.pe>
2024-06-23 17:34:34 -0500

    Actualizacion 1 del ejericicio 1: Comprobando el codigo de la pizarra

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 //Problema 1
 int main (){
-    cout<<"Hola mundo";
+    int op1, op2,r;
+    char op;
+    cin>>op1>>op2>>op;
+    switch(op){
+        case '+':
+        r=op1+op2; break;
+        case '-':
+        r=op1-op2; break;
+        case '*':
+        r=op1*op2; break;
+        case '/':
+        r=op1/op2; break;
+    }
+    cout<<r;
 }
