79f2633528a3f5a494aa48b9eed870d2251b7b88
lquispele <lquispele@unjbg.edu.pe>
2024-06-23 18:05:49 -0500

    Actualizacion 2 del programa 1: Mejora de condicionales e interfaz con penalizacion de tres intentos.

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 //Problema 1
 int main (){
-    int op1, op2,r;
+    int op1, op2,r,r1,i=0;
     char op;
-    cin>>op1>>op2>>op;
+    do{
+    cout<<"Ingrese los operadores antes de hacer la operacion:\n";
+    cin>>op1>>op2;
+    cout<<"(+)suma\n(-)resta\n(*)multiplicacion\n(/)division \nIngrese la simbologia que usara: ";
+    cin>>op;
     switch(op){
         case '+':
-        r=op1+op2; break;
+        r=op1+op2; 
+        cout<<"La suma es: "<<r<<endl;sleep(4);
+        system("clear");break;
         case '-':
-        r=op1-op2; break;
+        r=op1-op2; 
+        r1=op2-op1; 
+        cout<<" Las restas son: "<<r<<" y "<<r1<<endl;sleep(4);
+        system("clear");break;
         case '*':
-        r=op1*op2; break;
+        r=op1*op2; 
+        cout<<"La multiplicacion es: "<<r<<endl;sleep(4);
+        system("clear");break;
         case '/':
-        r=op1/op2; break;
+        if(op!=0){
+        r=op1/op2;
+        cout<<"La division es: "<<r<<endl;sleep(4);
+        system("clear");break;
+        } else{
+            cout<<"Es indeterminado."<<endl;sleep(4);
+            system("clear");break;
+        }
+        default:
+        cout<<"Ingrese otra opcion, porfavor."<<endl; sleep(2); 
+        system("clear");i++;break;
     }
-    cout<<r;
+    }while (i!=3);
+    cout<<"Espere 5 segundos...";
+    sleep(5);
+    cout<<"Gracias por usar el programa. Usted supero el limite de intentos.";
 }
