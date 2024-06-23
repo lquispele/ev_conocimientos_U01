37cdb9fa0c506f1d95ba0676ed971635eaf83337
lquispele <lquispele@unjbg.edu.pe>
2024-06-23 15:28:56 -0500

    Actualizacion 2 del ejercicio 2: Mejore las entradas y aniadi los divisores  comunes para que se muestren en la pantalla.

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -2,24 +2,31 @@
 #include<unistd.h>
 using namespace std;
 int main(){
-    int n1,n2,c=0, i=1;
+    int n1,n2,c=0, i=1, j=1;
     do{
         cout<<"Ingrese lo numeros que van a ser comparados: \n";
         cin>>n1;    //el primer numero
         cin>>n2;    // el segundo numero
+        cout<<"Los divisores comunes de "<<n1<<" y "<<n2<<" : ";
 
     while(i<=n1&&i<=n2){
         if(n1%i==0&&n2%i==0){
+            if(n1==i){
+                cout<<i<<" "<<endl;
+            }else{
+                cout<<i<<" ";
+            }
             c++;
         }
         i++;
+        j++;
     }
     if(c>1){
-        cout<<"No son PESI"<<endl;
+        cout<<"\nNo son PESI"<<endl;
     } else{
-        cout<<"Son PESI"<<endl;
+        cout<<"\nSon PESI"<<endl;
     }
-    sleep(2);
+    sleep(5);
     system("clear");
     }while (n1>0&&n2>0);
 }
